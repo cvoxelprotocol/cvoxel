e12bc051b1f5731016ad8350568fba0b130c403a
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-03-16 14:17:07 +0900

    fix:update sig request ui for sp

--- src/components/presenters/requests/SigRequestItem.tsx
+++ src/components/presenters/requests/SigRequestItem.tsx
@@ -1,8 +1,12 @@
+import { useWalletAccount } from "@/hooks/useWalletAccount";
 import { CVoxelMetaDraft } from "@/interfaces";
+import { getExploreLink } from "@/utils/etherscanUtils";
 import { formatBigNumber } from "@/utils/ethersUtil";
 import { shortHash } from "@/utils/tools";
 import { FC, useMemo } from "react";
 import { SigButton } from "../common/button/SigButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
 
 type SigRequestItemProps = {
     tx: CVoxelMetaDraft
@@ -11,16 +15,21 @@ type SigRequestItemProps = {
 }
 
 export const SigRequestItem:FC<SigRequestItemProps> = ({tx, account, handleClick}) => {
+    const {chainId} = useWalletAccount()
 
     const isPayee = useMemo(() => {
         return account?.toLowerCase()===tx.to.toLowerCase()
     },[account, tx])
 
+    const exploreLink = useMemo(() => {
+        return getExploreLink(tx.txHash, chainId)
+    },[tx,chainId])
+
 
     return (
-        <div className="rounded-lg shadow-lg bg-white dark:bg-card text-sm text-black dark:text-white break-words flex-wrap font-medium">
-            <div className="max-h-[90px]  flex justify-between items-center  py-2 px-4 border-b border-b-gray-200" >
-                <div className="flex justify-evenly items-center">
+        <div className="rounded-lg shadow-lg bg-white dark:bg-card text-xs sm:text-sm text-black dark:text-white break-words flex-wrap font-medium">
+            <div className="h-fit sm:max-h-[90px] sm:flex justify-between items-center py-2 px-2 sm:px-4 border-b border-b-gray-200" >
+                <div className="flex justify-evenly items-center pb-2 sm:pb-0">
                     <div className="max-w-[120px] px-4">
                         <p className="text-gray-500">{isPayee ? "from": "to"}</p>
                         <p className="break-words flex-wrap">{isPayee ? shortHash(tx.from, 13) : shortHash(tx.to, 13)}</p>
@@ -36,12 +45,16 @@ export const SigRequestItem:FC<SigRequestItemProps> = ({tx, account, handleClick
                     <div className="w-[0.5px] bg-black border-black h-[40px]"></div>
                     <div className="max-w-[120px] px-4">
                         <p className="text-gray-500">Tx Hash</p>
-                        <p className="break-words flex-wrap">{shortHash(tx.txHash, 13)}</p>
+                        <p className="break-words flex-wrap">{shortHash(tx.txHash, 8)}</p>
+                        <a className="flex items-center justify-center" href={exploreLink} target="_blank" rel="noreferrer">
+                            <p className="text-xs text-gray-500">explorer</p>
+                            <FontAwesomeIcon className="w-2 h-2 ml-1" icon={faExternalLink} color={'gray'}/>
+                        </a>
                     </div>
-                    <div className="w-[0.5px] bg-black border-black h-[40px]"></div>
+                    <div className="hidden sm:block w-[0.5px] bg-black border-black h-[40px]"></div>
                 </div>
-                <div className="max-w-[120px] px-2">
-                    <p className="text-md font-bold">
+                <div className="max-w-full sm:max-w-[120px] px-2">
+                    <p className="text-lg sm:text-sm font-bold">
                         {formatBigNumber(tx.value, 6)} {tx.tokenSymbol ? tx.tokenSymbol: "ETH"}
                     </p>
                 </div>
