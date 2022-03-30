02f9b642f2a958b9076a76a22d7e7afd700e3578
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-03-30 15:28:17 +0900

    update:loading modal UI

--- src/components/common/LoadingModal.tsx
+++ src/components/common/LoadingModal.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react'
+import Lottie from 'react-lottie-player'
+import loadingJson from './cvoxel_loading.json'
 
 type Props = {
     text?: string
@@ -6,13 +8,18 @@ type Props = {
 export const LoadingModal:FC<Props> = ({text}) => {
 
     return (
-        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-ground opacity-90 flex flex-col items-center justify-center">
+        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white bg-opacity-50 flex flex-col items-center justify-center">
             <div className="flex justify-center items-center">
-                <div
-                    className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-primary"
-                ></div>
+                <Lottie
+                    loop
+                    animationData={loadingJson}
+                    play
+                    style={{ width: 200, height: 200 }}
+                />;
             </div>
-            <p className="text-white text-sm md:text-lg pt-4 px-4">{text || ""}</p>
+            <p className="text-black font-bold text-xl md:text-2xl">{text || ""}</p>
         </div>
     )
+
+    
 }
