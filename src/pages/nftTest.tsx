1cde7438fcd3c6d02fd6eeff99b12c13ceb292f3
Kouki Minamoto <tomatomusicstudio@gmail.com>
2022-04-12 20:56:08 +0900

    Pull staging

---
+++ src/pages/nftTest.tsx
@@ -0,0 +1,12 @@
+import NFTVisualizer from "@/components/CVoxel/NFTVisualizer";
+import { NextPage } from "next";
+
+const NFTTest:NextPage = () => {
+  return (
+    <>
+      <NFTVisualizer />
+    </>
+  )
+}
+
+export default NFTTest;
