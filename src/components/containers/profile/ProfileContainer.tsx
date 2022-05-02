5d237ae3d7c982a2e37d98cc7fd7ac6613f02afa
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-05-02 16:56:19 +0900

    update: improved rendering function on cvoxel's list

--- src/components/containers/profile/ProfileContainer.tsx
+++ src/components/containers/profile/ProfileContainer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import { CeramicProps } from "@/interfaces/ceramic";
 import { useCVoxelsRecord } from "@/hooks/useCVoxel";
@@ -8,6 +8,7 @@ import CVoxelsPresenter from "@/components/CVoxel/CVoxelsPresenter";
 import { CVoxelItem } from "@/components/CVoxel/CVoxelItem";
 import { NoItemPresenter } from "@/components/common/NoItemPresenter";
 import { ProfileCard } from "@/components/Profile/ProfileCard";
+import { CommonLoading } from "@/components/common/CommonLoading";
 
 export const ProfileContainer: FC<CeramicProps> = ({ did }) => {
 
@@ -24,30 +25,37 @@ export const ProfileContainer: FC<CeramicProps> = ({ did }) => {
     }
   },[did, setUserProfile])
 
+  const CVoxelsPresenterMemo = useMemo(() => <CVoxelsPresenter>
+    {CVoxelsRecords.isLoading && (
+      <CommonLoading />
+    )}
+    {!CVoxelsRecords.isLoading && CVoxelsRecords.content?.cVoxels &&
+      CVoxelsRecords.content.cVoxels.map((item) => {
+        return <CVoxelItem did={did} item={item} key={item.id} isOwner={false} />;
+      })}
+    {!CVoxelsRecords.isLoading && !CVoxelsRecords.content && (
+      <div className="mx-auto">
+        <NoItemPresenter text="No C-Voxels yet..." />
+    </div>
+    )}
+  </CVoxelsPresenter>, [CVoxelsRecords.isLoading, CVoxelsRecords.content,did])
+
+  const VisualizerPresenterMemo = useMemo(() => <Canvas shadows>
+    <VisualizerPresenter
+      ids={CVoxelsRecords.content ? CVoxelsRecords.content.cVoxels.map((vox) => vox.id): []}
+    />
+  </Canvas>,[CVoxelsRecords.content])
+
   return (
     <main className="h-auto overflow-y-scroll text-black dark:text-white text-center">
       <div className="flex flex-col items-center w-full h-full pb-12">
-      <div className="flex w-full items-center justify-center h-[450px] relative max-w-[720px]">
-          <Canvas shadows>
-            <VisualizerPresenter
-              ids={CVoxelsRecords.content ? CVoxelsRecords.content.cVoxels.map((vox) => vox.id): []}
-            />
-          </Canvas>
+        <div className="flex w-full items-center justify-center h-[450px] relative max-w-[720px]">
+          {VisualizerPresenterMemo}
         </div>
         <div className="flex-none mb-12 w-full max-w-[720px]">
           <ProfileCard did={did} name={name} avator={avator} isLoading={profileRecord.isLoading} />
         </div>
-        <CVoxelsPresenter>
-          {CVoxelsRecords.content?.cVoxels &&
-            CVoxelsRecords.content.cVoxels.map((item) => {
-              return <CVoxelItem did={did} holder={name} item={item} key={item.id} isOwner={false} />;
-            })}
-          {!CVoxelsRecords.content && (
-            <div className="mx-auto">
-              <NoItemPresenter text="No C-Voxels yet..." />
-          </div>
-          )}
-        </CVoxelsPresenter>
+        {CVoxelsPresenterMemo}
       </div>
     </main>
   );
