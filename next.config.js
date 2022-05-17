d00eb2c2a1c56428a580c59a6018fd9a0babe9fd
hourglasshoro <hourglasshoro.628@gmail.com>
2022-05-18 00:55:12 +0900

    add dependencies

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['joeschmoe.io', 'ipfs.io', 'ipfs.infura.io' ],
+    domains: ['joeschmoe.io', 'ipfs.io', 'ipfs.infura.io' ]
   },
   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+
     return config;
   }
 }
