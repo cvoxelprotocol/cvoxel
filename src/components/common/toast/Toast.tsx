f94894e0f62530b9ac032e5b248f91ff45c1d15b
hourglasshoro <hourglasshoro.628@gmail.com>
2022-05-18 10:18:38 +0900

    format

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,19 +1,19 @@
-import {FC, useEffect, useRef, useState} from "react";
-import ToastBase from "@/components/common/toast/toast.svg"
-import {CVoxelThree} from "@/interfaces";
+import { FC, useEffect, useRef, useState } from "react";
+import ToastBase from "@/components/common/toast/toast.svg";
+import { CVoxelThree } from "@/interfaces";
 import CVoxelPresenter from "@/components/CVoxel/CVoxelPresenter";
-import {Canvas, useFrame} from "@react-three/fiber";
-import {OrbitControls, PerspectiveCamera} from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import * as THREE from "three";
-import {useCVoxelToast} from "@/hooks/useCVoxelToast";
+import { useCVoxelToast } from "@/hooks/useCVoxelToast";
 import clsx from "clsx";
 
 type Props = {
-  message: string
-  voxel: CVoxelThree
-}
+  message: string;
+  voxel: CVoxelThree;
+};
 
-const Presenter = ({voxel}: { voxel: CVoxelThree }) => {
+const Presenter = ({ voxel }: { voxel: CVoxelThree }) => {
   const cCollectionRef = useRef<THREE.Group>(new THREE.Group());
 
   useFrame(() => {
@@ -22,7 +22,7 @@ const Presenter = ({voxel}: { voxel: CVoxelThree }) => {
 
   return (
     <>
-      <ambientLight intensity={1.0}/>
+      <ambientLight intensity={1.0} />
       <directionalLight
         castShadow
         position={[0, 8, 0]}
@@ -35,39 +35,50 @@ const Presenter = ({voxel}: { voxel: CVoxelThree }) => {
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
       />
-      <OrbitControls enablePan={false} enableZoom={false} enableRotate={false}/>
+      <OrbitControls
+        enablePan={false}
+        enableZoom={false}
+        enableRotate={false}
+      />
       <group ref={cCollectionRef} position={[0, -1.7, 0]}>
-        <CVoxelPresenter {...{
-          ...voxel,
-          position: new THREE.Vector3(0, 0, 0),
-          offset: new THREE.Vector3(0, 0, 0),
-          scale: 1.0
-        }}/>
+        <CVoxelPresenter
+          {...{
+            ...voxel,
+            position: new THREE.Vector3(0, 0, 0),
+            offset: new THREE.Vector3(0, 0, 0),
+            scale: 1.0,
+          }}
+        />
       </group>
-      <PerspectiveCamera makeDefault position={[10, 6, 10]} zoom={3}/>
+      <PerspectiveCamera makeDefault position={[10, 6, 10]} zoom={3} />
     </>
-  )
-}
+  );
+};
 
-export const Toast: FC<Props> = ({message, voxel}) => {
-  const {isToastShow} = useCVoxelToast()
-  const [isMount, setIsMount] = useState<boolean>(false)
+export const Toast: FC<Props> = ({ message, voxel }) => {
+  const { isToastShow } = useCVoxelToast();
+  const [isMount, setIsMount] = useState<boolean>(false);
 
   useEffect(() => {
     // NOTE: Prevent fade out in the initial display and display nothing
     if (isToastShow) {
-      setIsMount(true)
+      setIsMount(true);
     }
-  }, [isToastShow])
+  }, [isToastShow]);
 
   return (
     <div className="fixed flex flex-col items-center w-full z-50 pointer-events-none">
       <div
-        className={clsx("relative overflow-hidden", !isMount && "hidden", isToastShow ? "animate-slide-in" : "animate-fade-out")}>
-        <ToastBase className="w-32"/>
+        className={clsx(
+          "relative overflow-hidden",
+          !isMount && "hidden",
+          isToastShow ? "animate-slide-in" : "animate-fade-out",
+        )}
+      >
+        <ToastBase className="w-32" />
         <div className="absolute left-0 right-0 bottom-0 text-center mb-8">
           <Canvas>
-            <Presenter voxel={voxel}/>
+            <Presenter voxel={voxel} />
           </Canvas>
           <p className="text-sm text-primary font-medium inline-block text-ellipsis overflow-hidden max-w-toast">
             {message}
@@ -75,5 +86,5 @@ export const Toast: FC<Props> = ({message, voxel}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,125 +1,134 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const plugin = require('tailwindcss/plugin')
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/constants/**/*.{js,ts}'],
-    darkMode: 'class',
-    theme: {
-        fontFamily: {
-            'sans': ['Work Sans', ...defaultTheme.fontFamily.sans]
-        },
-
-        extend: {
-            backgroundColor: theme => ({
-                'darkgray': '#24242B',
-                'appbar': '#18181D'
-            }),
-            textColor: theme => ({
-                'subwhite': '#8899A6',
-                'logo': '#F1F1F3',
-                'basegray': '#C5C5C5'
-            }),
-            colors: {
-                // Unique Colors
-                'primary': "#8E477F",
-                'primary-300': "#A66497",
-                'primary-light': '#3d6be2',
-                'secondary': '#EFA9E0',
-                'secondary-light': '#ED74E1',
-
-                // Neutral Colors
-                'ground': '#24242B',
-                'glass': '#303034',
-                'glass-stroke': '#505050',
-                'onglass': '#C5C5C5',
-                'onglass-weak': '#A0A0A0',
-                'acrylic': '#2A2A2D',
-                'acrylic-light': '#3A3A40',
-                'onacrylic': '#C5C5C5',
-                'card': '#44444E',
-                'oncard': '#FFFFFF',
-
-                // Gradient factors
-                'border_l': '#D88080',
-                'border_r': '#DB85C8',
-
-                // Gradient factors
-                'accent_l': '#459BFF',
-                'accent_r': '#68DFD8',
-
-                // Specific Colors
-                'done': '#33C102',
-                'progress': '#D4BD1B',
-                'suspended': '#AF1E1E',
-                text: {
-                    primary: "#8E477F",
-                    white: "#FFFFFF"
-                },
-            },
-            dropShadow: {
-                'base': '4px 4px 2px rgba(0, 0, 0, 0.25)',
-                'high': '10px 10px 10px rgba(0, 0, 0, 0.5)',
-                'higher': '20px 20px 20px rgba(0, 0, 0, 0.5)',
-                'xl-white': '0 20px 13px rgba(255, 255, 255, 1.0)',
-                'xl-pink': '0 2px 7px rgba(239, 169, 224, 1.0)'
-            },
-            maxHeight: {
-                '4/5': '80%',
-            },
-            minHeight: {
-                "44": "11rem",
-                "24": "6rem"
-            },
-            maxWidth: {
-                "modal": "708px",
-                "toast": "4rem"
-            },
-            animation: {
-                'slide-in': 'slide-in 0.5s ease forwards',
-                'fade-out': 'fade-out 0.5s ease forwards'
-            },
-            keyframes: {
-                'slide-in': {
-                    '0%': {transform: 'translateY(-50%)', opacity: 0},
-                    '100%': {transform: 'translateY(0)', opacity: 1}
-                },
-                'fade-out': {
-                    '0%': {transform: 'scale(1)', opacity: 1},
-                    '100%': {transform: 'scale(0.2)', opacity: 0}
-                }
-            }
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/constants/**/*.{js,ts}",
+  ],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      sans: ["Work Sans", ...defaultTheme.fontFamily.sans],
     },
-    variants: {
-        extend: {},
+
+    extend: {
+      backgroundColor: (theme) => ({
+        darkgray: "#24242B",
+        appbar: "#18181D",
+      }),
+      textColor: (theme) => ({
+        subwhite: "#8899A6",
+        logo: "#F1F1F3",
+        basegray: "#C5C5C5",
+      }),
+      colors: {
+        // Unique Colors
+        primary: "#8E477F",
+        "primary-300": "#A66497",
+        "primary-light": "#3d6be2",
+        secondary: "#EFA9E0",
+        "secondary-light": "#ED74E1",
+
+        // Neutral Colors
+        ground: "#24242B",
+        glass: "#303034",
+        "glass-stroke": "#505050",
+        onglass: "#C5C5C5",
+        "onglass-weak": "#A0A0A0",
+        acrylic: "#2A2A2D",
+        "acrylic-light": "#3A3A40",
+        onacrylic: "#C5C5C5",
+        card: "#44444E",
+        oncard: "#FFFFFF",
+
+        // Gradient factors
+        border_l: "#D88080",
+        border_r: "#DB85C8",
+
+        // Gradient factors
+        accent_l: "#459BFF",
+        accent_r: "#68DFD8",
+
+        // Specific Colors
+        done: "#33C102",
+        progress: "#D4BD1B",
+        suspended: "#AF1E1E",
+        text: {
+          primary: "#8E477F",
+          white: "#FFFFFF",
+        },
+      },
+      dropShadow: {
+        base: "4px 4px 2px rgba(0, 0, 0, 0.25)",
+        high: "10px 10px 10px rgba(0, 0, 0, 0.5)",
+        higher: "20px 20px 20px rgba(0, 0, 0, 0.5)",
+        "xl-white": "0 20px 13px rgba(255, 255, 255, 1.0)",
+        "xl-pink": "0 2px 7px rgba(239, 169, 224, 1.0)",
+      },
+      maxHeight: {
+        "4/5": "80%",
+      },
+      minHeight: {
+        44: "11rem",
+        24: "6rem",
+      },
+      maxWidth: {
+        modal: "708px",
+        toast: "4rem",
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease forwards",
+        "fade-out": "fade-out 0.5s ease forwards",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateY(-50%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        "fade-out": {
+          "0%": { transform: "scale(1)", opacity: 1 },
+          "100%": { transform: "scale(0.2)", opacity: 0 },
+        },
+      },
     },
-    plugins: [plugin(function ({addUtilities}) {
-        addUtilities({
-            '.scrollbar-hide': {
-                /* IE and Edge */
-                '-ms-overflow-style': 'none',
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          ".scrollbar-hide": {
+            /* IE and Edge */
+            "-ms-overflow-style": "none",
 
-                /* Firefox */
-                'scrollbar-width': 'none',
+            /* Firefox */
+            "scrollbar-width": "none",
 
-                /* Safari and Chrome */
-                '&::-webkit-scrollbar': {
-                    display: 'none'
-                }
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "none",
             },
+          },
 
-            '.scrollbar-default': {
-                /* IE and Edge */
-                '-ms-overflow-style': 'auto',
+          ".scrollbar-default": {
+            /* IE and Edge */
+            "-ms-overflow-style": "auto",
 
-                /* Firefox */
-                'scrollbar-width': 'auto',
+            /* Firefox */
+            "scrollbar-width": "auto",
 
-                /* Safari and Chrome */
-                '&::-webkit-scrollbar': {
-                    display: 'block'
-                }
-            }
-        }, ['responsive'])
-    })],
+            /* Safari and Chrome */
+            "&::-webkit-scrollbar": {
+              display: "block",
+            },
+          },
+        },
+        ["responsive"],
+      );
+    }),
+  ],
 };
