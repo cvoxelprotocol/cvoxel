881e147014d5f894327962588772f83fafe6d046
hourglasshoro <hourglasshoro.628@gmail.com>
2022-05-23 18:30:44 +0900

    Fix TransactionItem

--- src/components/Transaction/TransactionItem.tsx
+++ src/components/Transaction/TransactionItem.tsx
@@ -1,17 +1,18 @@
 import { CVoxelItem, TransactionLogWithChainId } from "@/interfaces";
 import { getExploreLink } from "@/utils/etherscanUtils";
 import { formatBigNumber } from "@/utils/ethersUtil";
-import { shortHash } from "@/utils/tools";
 import { FC, useMemo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExternalLink,
+  faCircleCheck,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { CommonSpinner } from "../common/CommonSpinner";
 import { convertTimestampToDateStr } from "@/utils/dateUtil";
-import { NetworkChip } from "./NetworkChip";
 import { getNetworkSymbol } from "@/utils/networkUtil";
 import { selectTxType } from "../containers/home";
 import { useENS } from "@/hooks/useENS";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 type TransactionItemProps = {
   index: number;
@@ -61,55 +62,78 @@ export const TransactionItem: FC<TransactionItemProps> = ({
   };
 
   return (
-    <div className="w-full h-fit sm:max-h-[90px] rounded-lg shadow-lg bg-white dark:bg-card sm:flex justify-between items-start text-xs sm:text-sm text-black dark:text-white break-words flex-wrap py-2 px-4 border-b border-b-gray-200">
+    <div className="w-full h-fit sm:max-h-[90px] rounded-lg shadow-lg bg-white dark:bg-card justify-between items-start text-xs sm:text-sm text-black dark:text-white break-words flex-wrap py-2 px-4 border-b border-b-gray-200 sm:grid grid-cols-12 gap-2">
       <div className="h-full">
         <div className="text-xs text-primary">From</div>
         <div></div>
       </div>
-      <div className="flex flex-col">
-        <div className="flex-1 text-md text-primary font-medium">
-          Alice Green
+
+      <div className="flex flex-col items-start col-span-3 h-full">
+        <div className="flex-initial text-md text-primary font-medium">
+          {ensLoading ? (
+            <CommonSpinner size="sm" />
+          ) : (
+            <p className="break-words flex-wrap">{ens}</p>
+          )}
         </div>
-        <div className="flex-auto text-lg font-medium">1.5ETH</div>
+        <div className="text-lg font-medium flex-auto flex flex-col justify-center">
+          <p className="text-md font-semibold">
+            {formatBigNumber(tx.value, 1, tx.tokenDecimal)}{" "}
+            {tx.tokenSymbol || getNetworkSymbol(tx.chainId)}
+          </p>
+        </div>
       </div>
-      <div>
-        <FontAwesomeIcon
-          className="w-3 h-3 ml-1"
-          icon={faCircleCheck}
-          color={"green"}
-        />
-      </div>
-      <div className="text-left">
-        <div>
-          <div className="text-2xs text-gray-500 font-semibold">Tx Hash</div>
+
+      <div className="text-left col-span-5 flex items-center">
+        <div className="flex-initial flex flex-col mr-3">
+          {/* TODO: duotone is only available in PRO and must be imported as an image. */}
+          <FontAwesomeIcon
+            className="w-3 h-3 ml-1 text-green-300"
+            icon={faCircleCheck}
+          />
+        </div>
+
+        <div className="mr-4 flex-auto overflow-hidden">
+          <div>
+            <div className="text-2xs text-gray-500 font-semibold">Tx Hash</div>
+            <div className="text-sm font-medium text-ellipsis overflow-hidden whitespace-nowrap w-full">
+              {tx.hash}
+            </div>
+          </div>
+          <div className="text-2xs text-gray-500 font-semibold">Timestamp</div>
           <div className="text-sm font-medium">
-            0x000000000000000000000000000000000
+            <p>{convertTimestampToDateStr(tx.timeStamp)}</p>
           </div>
         </div>
-        <div className="text-2xs text-gray-500 font-semibold">Timestamp</div>
-        <div className="text-sm font-medium">Dec 7 2022 11:15:17</div>
+
+        <div>
+          <a
+            className="flex items-center justify-center"
+            href={exploreLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              className="w-4 h-4 ml-1 text-gray-400"
+              icon={faExternalLink}
+            />
+          </a>
+        </div>
       </div>
-      <div>
-        <a
-          className="flex items-center justify-center"
-          href={exploreLink}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            className="w-3 h-3 ml-1"
-            icon={faExternalLink}
-            color={"gray"}
-          />
-        </a>
-      </div>
-      <div className="flex items-center justify-end">
+
+      <div className="flex items-center justify-end col-span-3 h-full">
         {isSelecting ? (
           <button
             onClick={() => handleClick()}
             className="text-primary-300 rounded-full bg-white border border-primary-300 py-1.5 px-4"
           >
-            {"Close"}
+            <div className="flex items-center">
+              <FontAwesomeIcon
+                className="w-3 h-3 mr-1 text-primary"
+                icon={faXmark}
+              />
+              <p>Close</p>
+            </div>
           </button>
         ) : (
           <button
@@ -126,73 +150,5 @@ export const TransactionItem: FC<TransactionItemProps> = ({
         )}
       </div>
     </div>
-    // <div className="w-full h-fit sm:max-h-[90px] rounded-lg shadow-lg bg-white dark:bg-card sm:flex justify-between items-center text-xs sm:text-sm text-black dark:text-white break-words flex-wrap py-2 px-4 border-b border-b-gray-200">
-    //   <div className="text-center sm:flex sm:justify-evenly sm:items-center">
-    //     <div className="flex justify-evenly items-center pb-2 sm:pb-0">
-    //       <div className="max-w-[90px] sm:max-w-[120px] px-2">
-    //         <p className="text-md font-bold">
-    //           {formatBigNumber(tx.value, 6, tx.tokenDecimal)}{" "}
-    //           {tx.tokenSymbol || getNetworkSymbol(tx.chainId)}
-    //         </p>
-    //       </div>
-    //       <div className="w-[0.5px] bg-black border-black h-[40px]"></div>
-    //       <div className="max-w-[90px] sm:max-w-[120px] px-2 text-center">
-    //         <p className="text-gray-500">{isPayee ? "from" : "to"}</p>
-    //         {ensLoading ? (
-    //           <CommonSpinner size="sm" />
-    //         ) : (
-    //           <p className="break-words flex-wrap">{ens}</p>
-    //         )}
-    //       </div>
-    //       <div className="hidden sm:block w-[0.5px] bg-black border-black h-[40px]"></div>
-    //     </div>
-    //     <div className="flex justify-evenly items-center">
-    //       <div className="max-w-[90px] sm:max-w-[120px] px-2 text-center space-y-1">
-    //         <NetworkChip chainId={tx.chainId} />
-    //         <p>{convertTimestampToDateStr(tx.timeStamp)}</p>
-    //       </div>
-    //       <div className="w-[0.5px] bg-black border-black h-[40px]"></div>
-    //       <div className="max-w-[90px] sm:max-w-[120px] px-2">
-    //         <p className="text-gray-500">Tx Hash</p>
-    //         <p className="break-words flex-wrap">{shortHash(tx.hash, 8)}</p>
-    //         <a
-    //           className="flex items-center justify-center"
-    //           href={exploreLink}
-    //           target="_blank"
-    //           rel="noreferrer"
-    //         >
-    //           <p className="text-xs text-gray-500">explorer</p>
-    //           <FontAwesomeIcon
-    //             className="w-2 h-2 ml-1"
-    //             icon={faExternalLink}
-    //             color={"gray"}
-    //           />
-    //         </a>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="flex items-center justify-end">
-    //     {isSelecting ? (
-    //       <button
-    //         onClick={() => handleClick()}
-    //         className="text-primary-300 rounded-full bg-white border border-primary-300 py-1.5 px-4"
-    //       >
-    //         {"Close"}
-    //       </button>
-    //     ) : (
-    //       <button
-    //         onClick={() => handleClick()}
-    //         className={
-    //           "rounded-full py-1.5 px-4 " +
-    //           (!isAlreadyCreated
-    //             ? " text-white bg-primary-300 "
-    //             : "text-primary-300 bg-white border border-primary-300")
-    //         }
-    //       >
-    //         {isAlreadyCreated ? "Show Detail" : "Create"}
-    //       </button>
-    //     )}
-    //   </div>
-    // </div>
   );
 };
--- src/utils/dateUtil.ts
+++ src/utils/dateUtil.ts
@@ -1,11 +1,13 @@
 export const convertTimestampToDateStr = (timestamp: string): string => {
   const d = new Date(Number(timestamp) * 1000);
-  return formatDate(d.toISOString());
-};
-
-const formatDate = (dateStr: string): string => {
-  const d = dateStr.split("-");
-  return `${d[2].split("T")[0]}-${d[1]}-${d[0]}`;
+  d.getMonth();
+  const month = d.toLocaleString("en-US", { month: "short" });
+  const day = d.getDay();
+  const year = d.getFullYear();
+  const hours = String(d.getHours()).padStart(2, "0");
+  const minutes = String(d.getMinutes()).padStart(2, "0");
+  const seconds = String(d.getSeconds()).padStart(2, "0");
+  return `${month} ${day} ${year} ${hours}:${minutes}:${seconds}`;
 };
 
 export const convertDateToTimestampStr = (date: Date): string => {
