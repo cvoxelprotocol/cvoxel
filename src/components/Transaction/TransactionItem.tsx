a133d338be19c1c2868495f011474cc819719478
hourglasshoro <hourglasshoro.628@gmail.com>
2022-06-04 09:35:23 +0900

    fix: design fix

--- src/components/Transaction/TransactionItem.tsx
+++ src/components/Transaction/TransactionItem.tsx
@@ -5,14 +5,14 @@ import { FC, useMemo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faExternalLink,
-  faXmark,
+  faAngleDown,
+  faAngleUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { CommonSpinner } from "../common/CommonSpinner";
 import { convertTimestampToDateStr } from "@/utils/dateUtil";
 import { getNetworkSymbol } from "@/utils/networkUtil";
 import { selectTxType } from "../containers/home";
 import { useENS } from "@/hooks/useENS";
-import Check from "@/components/Transaction/check.svg";
 
 type TransactionItemProps = {
   index: number;
@@ -62,47 +62,54 @@ export const TransactionItem: FC<TransactionItemProps> = ({
   };
 
   return (
-    <div className="w-full h-fit sm:max-h-[90px] rounded-lg shadow-lg bg-white dark:bg-card justify-between items-start text-xs sm:text-sm text-black dark:text-white break-words flex-wrap py-2 px-4 border-b border-b-gray-200 sm:grid grid-cols-12 gap-2">
-      <div className="h-full">
-        <div className="text-xs text-primary">From</div>
-        <div></div>
-      </div>
-
-      <div className="flex flex-col items-start col-span-3 h-full">
-        <div className="flex-initial text-md text-primary font-medium">
-          {ensLoading ? (
-            <CommonSpinner size="sm" />
-          ) : (
-            <p className="break-words flex-wrap">{ens}</p>
-          )}
-        </div>
-        <div className="text-lg font-medium flex-auto flex flex-col justify-center">
-          <p className="text-md font-semibold">
-            {formatBigNumber(tx.value, 1, tx.tokenDecimal)}{" "}
-            {tx.tokenSymbol || getNetworkSymbol(tx.chainId)}
-          </p>
-        </div>
-      </div>
-
-      <div className="text-left col-span-5 flex items-center">
-        <div className="mr-4 flex-auto overflow-hidden pl-6 relative">
-          {/*TODO: when error or pending(?) to be invisible*/}
-          <div className="absolute top-0 bottom-0 left-0 flex flex-col justify-center">
-            <Check className="w-3 h-3"/>
+    <div className="w-full h-fit sm:max-h-[90px] rounded-lg shadow-lg bg-white dark:bg-card justify-between items-start text-xs sm:text-sm text-black dark:text-white break-words flex-wrap py-2 px-10 border-b border-b-gray-200 sm:grid grid-cols-12 gap-2">
+      {/*address and value*/}
+      <div className="flex col-span-3 h-full">
+        {/*content*/}
+        <div className="flex flex-col items-start w-full flex-auto">
+          <div className="h-full">
+            <div className="text-xs text-primary">From</div>
+            <div></div>
           </div>
+          <div className="flex-initial text-lg text-primary font-medium">
+            {ensLoading ? (
+              <CommonSpinner size="sm" />
+            ) : (
+              <p className="break-words flex-wrap">{ens}</p>
+            )}
+          </div>
+          <div className="text-xl font-medium flex-auto flex flex-col justify-center">
+            <p className="text-md font-semibold">
+              {formatBigNumber(tx.value, 1, tx.tokenDecimal)}{" "}
+              {tx.tokenSymbol || getNetworkSymbol(tx.chainId)}
+            </p>
+          </div>
+        </div>
+        {/*border*/}
+        <div className="w-0.5 bg-gray-200 flex-initial" />
+      </div>
 
+      {/*transaction*/}
+      <div className="text-left col-span-7 flex items-center">
+        <div className="mr-4 flex-auto overflow-hidden">
+          {/*hash*/}
           <div>
             <div className="text-2xs text-gray-500 font-semibold">Tx Hash</div>
             <div className="text-sm font-medium text-ellipsis overflow-hidden whitespace-nowrap w-full">
               {tx.hash}
             </div>
           </div>
-          <div className="text-2xs text-gray-500 font-semibold">Timestamp</div>
+
+          {/*timestamp*/}
+          <div className="text-2xs text-gray-500 font-semibold mt-2">
+            Timestamp
+          </div>
           <div className="text-sm font-medium">
             <p>{convertTimestampToDateStr(tx.timeStamp)}</p>
           </div>
         </div>
 
+        {/*external link*/}
         <div>
           <a
             className="flex items-center justify-center"
@@ -111,40 +118,25 @@ export const TransactionItem: FC<TransactionItemProps> = ({
             rel="noreferrer"
           >
             <FontAwesomeIcon
-              className="w-4 h-4 ml-1 text-gray-400"
+              className="w-4 h-4 ml-1 text-gray-400 hover:text-gray-300"
               icon={faExternalLink}
             />
           </a>
         </div>
       </div>
 
-      <div className="flex items-center justify-end col-span-3 h-full">
-        {isSelecting ? (
-          <button
-            onClick={() => handleClick()}
-            className="text-primary-300 rounded-full bg-white border border-primary-300 py-1.5 px-4"
-          >
-            <div className="flex items-center">
-              <FontAwesomeIcon
-                className="w-3 h-3 mr-1 text-primary"
-                icon={faXmark}
-              />
-              <p>Close</p>
-            </div>
-          </button>
-        ) : (
-          <button
-            onClick={() => handleClick()}
-            className={
-              "rounded-full py-1.5 px-4 " +
-              (!isAlreadyCreated
-                ? " text-white bg-primary-300 "
-                : "text-primary-300 bg-white border border-primary-300")
-            }
-          >
-            {isAlreadyCreated ? "Show Detail" : "Enter Context"}
-          </button>
-        )}
+      {/*spacer for grid*/}
+      <div className="col-span-1" />
+
+      <div className="flex items-center justify-end col-span-1 h-full">
+        <button onClick={() => handleClick()}>
+          <div className="flex items-center p-4">
+            <FontAwesomeIcon
+              className="w-5 h-5 mr-1 text-primary hover:text-secondary"
+              icon={isSelecting ? faAngleUp : faAngleDown}
+            />
+          </div>
+        </button>
       </div>
     </div>
   );
