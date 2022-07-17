94293efd28587199a1a8bf44511f9587128051bf
hourglasshoro <hourglasshoro.628@gmail.com>
2022-07-17 14:52:19 +0900

    fix: format

--- src/components/Profile/MyProfileIcon.tsx
+++ src/components/Profile/MyProfileIcon.tsx
@@ -5,33 +5,30 @@ import { CommonSpinner } from "../common/CommonSpinner";
 import { IconAvatar } from "../common/IconAvatar";
 
 type MyProfileCardProps = {
-  handleClick?: () => void
-}
-export const MyProfileIcon: FC<MyProfileCardProps> = ({handleClick}) => {
-  const {
-    connection,
-    did,
-    avator,
-  } = useMyCeramicAcount();
+  handleClick?: () => void;
+};
+export const MyProfileIcon: FC<MyProfileCardProps> = ({ handleClick }) => {
+  const { connection, did, avator } = useMyCeramicAcount();
 
-
-  if (connection.status==="connecting") {
+  if (connection.status === "connecting") {
     return (
       <div className="w-fit h-fit">
         <CommonSpinner />
       </div>
-    )
+    );
   }
 
   return (
     <div
       className={
-        "flex h-auto justify-center items-center rounded-full border w-fit p-1 border-secondary cursor-pointer"} >
-          {avator ? (
-            <IconAvatar size={"md"} src={avator} flex={false} />
-          ): (
-            <AvatarPlaceholder did={did} size={40} />
-          )}
-      </div>
+        "flex h-auto justify-center items-center rounded-full border w-fit p-1 border-secondary cursor-pointer"
+      }
+    >
+      {avator ? (
+        <IconAvatar size={"md"} src={avator} flex={false} />
+      ) : (
+        <AvatarPlaceholder did={did} size={40} />
+      )}
+    </div>
   );
 };
--- src/components/containers/home/MyPageContainer.tsx
+++ src/components/containers/home/MyPageContainer.tsx
@@ -8,41 +8,41 @@ import { MyTxContainer } from "./tabs/MyTxContainer";
 import { MyNotificationContainer } from "./tabs/MyNotificationContainer";
 
 type props = {
-    content?: CVoxels | null
-}
-export const MyPageContainer:FC<props> = ({content}) => {
-    const { tabState } = useTab();
+  content?: CVoxels | null;
+};
+export const MyPageContainer: FC<props> = ({ content }) => {
+  const { tabState } = useTab();
 
-    return (
-        <div className="relative w-full max-w-5xl min-h-screen border-t border-secondary mx-auto">
-          <div className="sm:flex sm:grid-cols-2">
-            <div className="sm:hidden w-full">
-              <HomeSPHeader />
-            </div>
-          <div className="hidden sm:block md:w-[280px] col-span-1">
-            <HomeTabsHeader />
+  return (
+    <div className="relative w-full max-w-5xl min-h-screen border-t border-secondary mx-auto">
+      <div className="sm:flex sm:grid-cols-2">
+        <div className="sm:hidden w-full">
+          <HomeSPHeader />
+        </div>
+        <div className="hidden sm:block md:w-[280px] col-span-1">
+          <HomeTabsHeader />
+        </div>
+        <div className="flex-none w-full sm:w-[calc(100%-95px)] md:w-[calc(100%-280px)] max-w-[744px] col-span-1">
+          <div
+            className={tabState === "cvoxels" ? "block" : "hidden"}
+            id="cvoxels"
+          >
+            <MyCVoxelContainer />
           </div>
-          <div className="flex-none w-full sm:w-[calc(100%-95px)] md:w-[calc(100%-280px)] max-w-[744px] col-span-1">
-            <div
-              className={tabState === "cvoxels" ? "block" : "hidden"}
-              id="cvoxels"
-            >
-              <MyCVoxelContainer />
-            </div>
-            <div
-              className={tabState === "transactions" ? "block" : "hidden"}
-              id="transactions"
-            >
-              <MyTxContainer />
-            </div>
-            <div
-              className={tabState === "notifications" ? "block" : "hidden"}
-              id="notifications"
-            >
-              <MyNotificationContainer />
-            </div>
+          <div
+            className={tabState === "transactions" ? "block" : "hidden"}
+            id="transactions"
+          >
+            <MyTxContainer />
           </div>
+          <div
+            className={tabState === "notifications" ? "block" : "hidden"}
+            id="notifications"
+          >
+            <MyNotificationContainer />
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
