983ce3d0b4b345a96ab55b43c6886c17462b3199
hourglasshoro <hourglasshoro.628@gmail.com>
2022-08-09 19:39:04 +0900

    fix: transaction title padding

--- src/components/Transaction/TxTabs.tsx
+++ src/components/Transaction/TxTabs.tsx
@@ -19,10 +19,13 @@ export const TxTabs: FC = () => {
   return (
     <div className="px-4 sm:px-0">
       <div className="w-full text-center border-b border-light-outline dark:border-dark-outline sm:mx-6 sm:h-20 flex flex-col justify-between items-start">
-        <div className="text-light-on-surface dark:text-dark-on-surface text-2xl font-medium px-2 py-1 hidden sm:block">
+        <div className="text-light-on-surface dark:text-dark-on-surface text-2xl font-medium py-1 hidden sm:block">
           Transactions
         </div>
-        <ul className="w-full flex items-center space-x-4 pt-4 sm:pt-0" role="txTablist">
+        <ul
+          className="w-full flex items-center space-x-4 pt-4 sm:pt-0"
+          role="txTablist"
+        >
           <li className="">
             <a
               className={clsx(
