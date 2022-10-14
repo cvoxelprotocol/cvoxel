6f714715f7d49295160ab74dc9dbed8a1d820c64
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2022-10-14 14:13:39 +0900

    fix layout

--- src/components/common/badge/TagBadge.tsx
+++ src/components/common/badge/TagBadge.tsx
@@ -5,7 +5,7 @@ type TagBadgeProps = {
 };
 export const TagBadge: FC<TagBadgeProps> = ({ text }) => {
   return (
-    <span className="inline-flex items-center justify-center px-3 py-1 my-0.5 mx-[4px] text-sm leading-none rounded-full text-light-on-primary-container dark:text-dark-on-primary-container bg-light-primary-container dark:bg-dark-primary-container font-medium">
+    <span className="whitespace-nowrap inline-flex items-center justify-center px-3 py-1 my-0.5 mx-[4px] text-xs sm:text-sm leading-none rounded-full text-light-on-primary-container dark:text-dark-on-primary-container bg-light-primary-container dark:bg-dark-primary-container font-medium">
       {text}
     </span>
   );
